4282b890cbf7da6d027d2a6969621fa92834d07d
wonderboy1w3 <jamshidkhujqulov@gmail.com>
2022-12-18 01:03:18 +0500

    SERVICE: Extension Methods

---
+++ src/AbuInt.API/Middlewares/CustomExceptionMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using AbuInt.Service.Exceptions;
+
+namespace AbuInt.API.Middlewares;
+
+public class CustomExceptionMiddleware
+{
+    private readonly RequestDelegate next;
+    private readonly ILogger<CustomExceptionMiddleware> logger;
+    public CustomExceptionMiddleware(RequestDelegate next, ILogger<CustomExceptionMiddleware> logger)
+    {
+        this.next = next;
+        this.logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await this.next.Invoke(context);
+        }
+        catch (CustomException ex)
+        {
+            await this.HandleException(context, ex.Code, ex.Message);
+        }
+        catch (Exception ex)
+        {
+            //Log
+            logger.LogError(ex.ToString());
+
+            await this.HandleException(context, 500, ex.Message);
+        }
+    }
+
+    public async Task HandleException(HttpContext context, int code, string message)
+    {
+        context.Response.StatusCode = code;
+        await context.Response.WriteAsJsonAsync(new
+        {
+            Code = code,
+            Message = message
+        });
+    }
+}
---
+++ src/AbuInt.Service/Extensions/CollectionExtension.cs
@@ -0,0 +1,24 @@
+﻿using AbuInt.Domain.Configuration;
+using AbuInt.Service.Extensions;
+using Newtonsoft.Json;
+
+namespace AbuInt.Service.Exceptions;
+
+public static class CollectionExtension
+{
+    public static IQueryable<T> ToPagedList<T>(this IQueryable<T> source, PaginationParams @params)
+    {
+        var metaData = new PaginationMetaData(source.Count(), @params);
+
+        var json = JsonConvert.SerializeObject(metaData);
+
+        if (HttpContextHelper.ResponseHeaders.ContainsKey("X-Pagination"))
+            HttpContextHelper.ResponseHeaders.Remove("X-Pagination");
+
+        HttpContextHelper.ResponseHeaders.Add("X-Pagination", json);
+
+        return @params.PageSize > 0 && @params.PageIndex >= 0
+            ? source.Skip((@params.PageIndex - 1) * @params.PageSize).Take(@params.PageSize)
+            : source;
+    }
+}
