82ebfad2cb6c99748b4d733f9ce917508a1702ff
Botirali <105649270+Rashidovicc@users.noreply.github.com>
2022-12-17 18:58:10 +0500

    FIX: Fixed QuizeResult.cs field

--- src/AbuInt.Domain/Entities/Quize/QuizeResult.cs
+++ src/AbuInt.Domain/Entities/Quize/QuizeResult.cs
@@ -4,13 +4,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AbuInt.Domain.Entities.Users;
 
 namespace AbuInt.Domain.Entities.Quize;
 
 public class QuizeResult : BaseEntity
 {
     public int UserId { get; set; }
-    // public User User { get; set; }
+    public User User { get; set; }
 
     public int QuizeId { get; set; }
     public Quize Quize { get; set; }
