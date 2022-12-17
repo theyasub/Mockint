0bd2e6df9cb462a3cf1cb9fb3b97f12445d31926
wonderboy1w3 <jamshidkhujqulov@gmail.com>
2022-12-17 19:04:57 +0500

    CODE RUB: Rename Quize folder to Quizes folder

--- src/AbuInt.Domain/Entities/Quizes/QuestionAnswer.cs
+++ src/AbuInt.Domain/Entities/Quizes/QuestionAnswer.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AbuInt.Domain.Entities.Quize;
+namespace AbuInt.Domain.Entities.Quizes;
 
 public class QuestionAnswer : Auditable
 {
